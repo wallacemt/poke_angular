a8fe0eb4d987a537105260d52c6f715e90edc3da
Wallace Santana <wallacesantanak0@gmail.com>
2025-06-18 19:59:38 -0300

    feat(pokeDetails): implememt pokemon details page with info and image

--- src/app/pages/pokemon-details/pokemon-details.page.ts
+++ src/app/pages/pokemon-details/pokemon-details.page.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonDetails } from 'src/app/models/pokemon.model';
+import { PokeApiService } from 'src/app/core/services/poke-api.service';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.page.html',
   styleUrls: ['./pokemon-details.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PokemonDetailsPage implements OnInit {
+  pokemonName!: string;
+  pokemonData!: PokemonDetails;
+  isLoading = true;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
 
   ngOnInit() {
+    this.pokemonName = this.route.snapshot.paramMap.get('name')!;
+    this.loadPokeDetails();
   }
 
+  loadPokeDetails() {
+    this.pokeApiService.getPokemonDetails(this.pokemonName).subscribe({
+      next: (data) => {
+        this.pokemonData = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log('Erro ao carregar detalhes do pokemon', error);
+        this.isLoading = false;
+      },
+    });
+  }
 }
