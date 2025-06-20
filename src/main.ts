9f26791386d9465824c0cff59c65f0282a1f41ca
Wallace Santana <wallacesantanak0@gmail.com>
2025-06-20 17:03:19 -0300

    build(./): update build settings

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy,  } from '@ionic/angular/standalone';
-
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import { IonicRouteStrategy } from '@ionic/angular/standalone';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
-import {HttpClientModule} from "@angular/common/http"
+import { HttpClientModule } from '@angular/common/http';
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
   ],
 });
